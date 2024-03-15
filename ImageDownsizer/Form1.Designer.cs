7020bb954942942ad35d162c7b5c789435ff3e33
slavcheww <temelko.eng@gmail.com>
2024-03-16 01:06:51 +0200

    add consequential downsizing function and time calculating feature

--- ImageDownsizer/Form1.Designer.cs
+++ ImageDownsizer/Form1.Designer.cs
@@ -37,32 +37,35 @@ namespace ImageDownsizer
             this.downscalePerLabel = new System.Windows.Forms.Label();
             this.consequentialButton = new System.Windows.Forms.Button();
             this.parallelButton = new System.Windows.Forms.Button();
+            this.bitmapToColorArrLbl = new System.Windows.Forms.Label();
+            this.resizingLbl = new System.Windows.Forms.Label();
+            this.colorArrToBitmapLbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.initialImageBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.downScaledImageBox)).BeginInit();
             this.SuspendLayout();
             // 
             // initialImageBox
             // 
-            this.initialImageBox.Location = new System.Drawing.Point(12, 12);
+            this.initialImageBox.Location = new System.Drawing.Point(15, 15);
             this.initialImageBox.Name = "initialImageBox";
-            this.initialImageBox.Size = new System.Drawing.Size(451, 166);
+            this.initialImageBox.Size = new System.Drawing.Size(565, 208);
             this.initialImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.initialImageBox.TabIndex = 0;
             this.initialImageBox.TabStop = false;
             // 
             // downScaledImageBox
             // 
-            this.downScaledImageBox.Location = new System.Drawing.Point(337, 293);
+            this.downScaledImageBox.Location = new System.Drawing.Point(422, 366);
             this.downScaledImageBox.Name = "downScaledImageBox";
-            this.downScaledImageBox.Size = new System.Drawing.Size(451, 145);
+            this.downScaledImageBox.Size = new System.Drawing.Size(565, 181);
             this.downScaledImageBox.TabIndex = 1;
             this.downScaledImageBox.TabStop = false;
             // 
             // uploadImageButton
             // 
-            this.uploadImageButton.Location = new System.Drawing.Point(574, 12);
+            this.uploadImageButton.Location = new System.Drawing.Point(718, 15);
             this.uploadImageButton.Name = "uploadImageButton";
-            this.uploadImageButton.Size = new System.Drawing.Size(161, 42);
+            this.uploadImageButton.Size = new System.Drawing.Size(201, 53);
             this.uploadImageButton.TabIndex = 2;
             this.uploadImageButton.Text = "Upload";
             this.uploadImageButton.UseVisualStyleBackColor = true;
@@ -70,24 +73,24 @@ namespace ImageDownsizer
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(89, 343);
+            this.saveButton.Location = new System.Drawing.Point(106, 479);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(161, 42);
+            this.saveButton.Size = new System.Drawing.Size(201, 53);
             this.saveButton.TabIndex = 3;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
             // 
             // downscalePerTextbox
             // 
-            this.downscalePerTextbox.Location = new System.Drawing.Point(545, 68);
+            this.downscalePerTextbox.Location = new System.Drawing.Point(681, 85);
             this.downscalePerTextbox.Name = "downscalePerTextbox";
-            this.downscalePerTextbox.Size = new System.Drawing.Size(109, 22);
+            this.downscalePerTextbox.Size = new System.Drawing.Size(171, 22);
             this.downscalePerTextbox.TabIndex = 4;
             // 
             // downscalePerLabel
             // 
             this.downscalePerLabel.AutoSize = true;
-            this.downscalePerLabel.Location = new System.Drawing.Point(660, 71);
+            this.downscalePerLabel.Location = new System.Drawing.Point(858, 88);
             this.downscalePerLabel.Name = "downscalePerLabel";
             this.downscalePerLabel.Size = new System.Drawing.Size(102, 17);
             this.downscalePerLabel.TabIndex = 6;
@@ -95,27 +98,59 @@ namespace ImageDownsizer
             // 
             // consequentialButton
             // 
-            this.consequentialButton.Location = new System.Drawing.Point(574, 115);
+            this.consequentialButton.Location = new System.Drawing.Point(718, 144);
             this.consequentialButton.Name = "consequentialButton";
-            this.consequentialButton.Size = new System.Drawing.Size(161, 42);
+            this.consequentialButton.Size = new System.Drawing.Size(201, 53);
             this.consequentialButton.TabIndex = 7;
             this.consequentialButton.Text = "Consequential";
             this.consequentialButton.UseVisualStyleBackColor = true;
+            this.consequentialButton.Click += new System.EventHandler(this.consequentialButton_Click);
             // 
             // parallelButton
             // 
-            this.parallelButton.Location = new System.Drawing.Point(574, 175);
+            this.parallelButton.Location = new System.Drawing.Point(718, 219);
             this.parallelButton.Name = "parallelButton";
-            this.parallelButton.Size = new System.Drawing.Size(161, 42);
+            this.parallelButton.Size = new System.Drawing.Size(201, 53);
             this.parallelButton.TabIndex = 8;
             this.parallelButton.Text = "Parallel";
             this.parallelButton.UseVisualStyleBackColor = true;
+            // 
+            // bitmapToColorArrLbl
+            // 
+            this.bitmapToColorArrLbl.AutoSize = true;
+            this.bitmapToColorArrLbl.Location = new System.Drawing.Point(15, 345);
+            this.bitmapToColorArrLbl.Name = "bitmapToColorArrLbl";
+            this.bitmapToColorArrLbl.Size = new System.Drawing.Size(145, 17);
+            this.bitmapToColorArrLbl.TabIndex = 9;
+            this.bitmapToColorArrLbl.Text = "Bitmap to Collor Array";
+            // 
+            // resizingLbl
+            // 
+            this.resizingLbl.AutoSize = true;
+            this.resizingLbl.Location = new System.Drawing.Point(15, 381);
+            this.resizingLbl.Name = "resizingLbl";
+            this.resizingLbl.Size = new System.Drawing.Size(62, 17);
+            this.resizingLbl.TabIndex = 10;
+            this.resizingLbl.Text = "Resizing";
+            // 
+            // colorArrToBitmapLbl
+            // 
+            this.colorArrToBitmapLbl.AutoSize = true;
+            this.colorArrToBitmapLbl.Location = new System.Drawing.Point(15, 418);
+            this.colorArrToBitmapLbl.Name = "colorArrToBitmapLbl";
+            this.colorArrToBitmapLbl.Size = new System.Drawing.Size(142, 17);
+            this.colorArrToBitmapLbl.TabIndex = 11;
+            this.colorArrToBitmapLbl.Text = "Color Array to Bitmap";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1021, 602);
+            this.Controls.Add(this.colorArrToBitmapLbl);
+            this.Controls.Add(this.resizingLbl);
+            this.Controls.Add(this.bitmapToColorArrLbl);
             this.Controls.Add(this.parallelButton);
             this.Controls.Add(this.consequentialButton);
             this.Controls.Add(this.downscalePerLabel);
@@ -124,6 +159,7 @@ namespace ImageDownsizer
             this.Controls.Add(this.uploadImageButton);
             this.Controls.Add(this.downScaledImageBox);
             this.Controls.Add(this.initialImageBox);
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.initialImageBox)).EndInit();
@@ -143,6 +179,9 @@ namespace ImageDownsizer
         private System.Windows.Forms.Label downscalePerLabel;
         private System.Windows.Forms.Button consequentialButton;
         private System.Windows.Forms.Button parallelButton;
+        private System.Windows.Forms.Label bitmapToColorArrLbl;
+        private System.Windows.Forms.Label resizingLbl;
+        private System.Windows.Forms.Label colorArrToBitmapLbl;
     }
 }
 
--- ImageDownsizer/Form1.cs
+++ ImageDownsizer/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
@@ -42,6 +43,76 @@ namespace ImageDownsizer
                     }
                 }
             }
+        }
+
+        private Tuple<long, long, long> ProcessBilinearSequential(Bitmap selectedImage, double downscaleFactor)
+        {
+            long bitmapToColorArrayTime, resizeTime, colorArrayToBitmapTime;
+
+            var colorArrayResult = MeasureExecutionTime(() =>
+            {
+                var array = BitmapHelper.BitmapToColorArray(selectedImage);
+                return array;
+            }, out bitmapToColorArrayTime);
+
+            var resizedImageResult = MeasureExecutionTime(() =>
+            {
+                var image = BilinearInterpolator.ResizeImageSequential(colorArrayResult, downscaleFactor);
+                return image;
+            }, out resizeTime);
+
+            var result = MeasureExecutionTime(() =>
+            {
+                var resultBitmap = BitmapHelper.ColorArrayToBitmap(resizedImageResult);
+                downScaledImageBox.Image = resultBitmap;
+                return resultBitmap;
+            }, out colorArrayToBitmapTime);
+
+            return Tuple.Create(bitmapToColorArrayTime, resizeTime, colorArrayToBitmapTime);
+        }
+
+        private T MeasureExecutionTime<T>(Func<T> action, out long executionTime)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var result = action();
+            stopwatch.Stop();
+            executionTime = stopwatch.ElapsedMilliseconds;
+            return result;
+        }
+
+        private void Downsize(bool isConsequential)
+        {
+            if (selectedImage == null)
+            {
+                MessageBox.Show("You have to select an image!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double downscalePer;
+
+            if (!double.TryParse(downscalePerTextbox.Text, out downscalePer) || downscalePer <= 0 || downscalePer > 100)
+            {
+                MessageBox.Show("Please enter a valid percantage (1-100).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            long bitmapToCollorArrResult = 0;
+            long resizeResult = 0;
+            long collorArrayToBitmapTime = 0;
+
+            if (isConsequential)
+            {
+                (bitmapToCollorArrResult, resizeResult, collorArrayToBitmapTime) = ProcessBilinearSequential(selectedImage, downscalePer);
+            }
+
+            bitmapToColorArrLbl.Text = bitmapToCollorArrResult.ToString();
+            resizingLbl.Text = resizeResult.ToString();
+            colorArrToBitmapLbl.Text = collorArrayToBitmapTime.ToString();
+        }
+
+        private void consequentialButton_Click(object sender, EventArgs e)
+        {
+            Downsize(true);
         }
     }
 }
