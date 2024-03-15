1343b8e1caafa0fa7afd622aa184329766357f7e
slavcheww <temelko.eng@gmail.com>
2024-03-15 15:19:42 +0200

    add BilinearInterpolator - method ResizeImageParallel

--- ImageDownsizer/BilinearInterpolator.cs
+++ ImageDownsizer/BilinearInterpolator.cs
@@ -62,5 +62,63 @@ namespace ImageDownsizer
 
             return resultImage;
         }
+
+        public static Color[,] ResizeImageParallel(Color[,] image, double scalePercent)
+        {
+            int initialHeight = image.GetLength(1);
+            int initialWidth = image.GetLength(0);
+
+            double scale = scalePercent / 100.0;
+            int updatedHeight = (int)(initialHeight * scale);
+            int updatedWidth = (int)(initialWidth * scale);
+
+            Color[,] resultImage = new Color[updatedWidth, updatedHeight];
+
+            double heightRatio = (double)(initialHeight - 1) / (updatedHeight - 1);
+            double widthRatio = (double)(initialWidth - 1) / (updatedWidth - 1);
+
+            // Parallelize the row processing
+            Parallel.For(0, updatedHeight, updatedY =>
+            {
+                // Process each parallely
+                for (int updatedX = 0; updatedX < updatedWidth; updatedX++)
+                {
+                    double sourceX = updatedX * widthRatio;
+                    double sourceY = updatedY * heightRatio;
+
+                    int x1 = (int)Math.Floor(sourceX);
+                    int x2 = Math.Min(x1 + 1, initialWidth - 1);
+                    int y1 = (int)Math.Floor(sourceY);
+                    int y2 = Math.Min(y1 + 1, initialHeight - 1);
+
+                    double xFraction = sourceX - x1;
+                    double yFraction = sourceY - y1;
+
+                    Color c11 = image[x1, y1];
+                    Color c12 = image[x1, y2];
+                    Color c21 = image[x2, y1];
+                    Color c22 = image[x2, y2];
+
+                    byte red = (byte)((1 - xFraction) * (1 - yFraction) * c11.R +
+                                    xFraction * (1 - yFraction) * c21.R +
+                                    (1 - xFraction) * yFraction * c12.R +
+                                    xFraction * yFraction * c22.R);
+
+                    byte green = (byte)((1 - xFraction) * (1 - yFraction) * c11.G +
+                                    xFraction * (1 - yFraction) * c21.G +
+                                    (1 - xFraction) * yFraction * c12.G +
+                                    xFraction * yFraction * c22.G);
+
+                    byte blue = (byte)((1 - xFraction) * (1 - yFraction) * c11.B +
+                                    xFraction * (1 - yFraction) * c21.B +
+                                    (1 - xFraction) * yFraction * c12.B +
+                                    xFraction * yFraction * c22.B);
+
+                    resultImage[updatedX, updatedY] = Color.FromArgb(red, green, blue);
+                }
+            });
+
+            return resultImage;
+        }
     }
 }
